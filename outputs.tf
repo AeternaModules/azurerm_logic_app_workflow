6612386b661f9c1336b6fc2ef153a74908778842
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:18:39 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,77 +1,73 @@
-output "logic_app_workflows" {
-  description = "All logic_app_workflow resources"
-  value       = azurerm_logic_app_workflow.logic_app_workflows
-}
 output "logic_app_workflows_access_control" {
-  description = "List of access_control values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.access_control]
+  description = "Map of access_control values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.access_control }
 }
 output "logic_app_workflows_access_endpoint" {
-  description = "List of access_endpoint values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.access_endpoint]
+  description = "Map of access_endpoint values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.access_endpoint }
 }
 output "logic_app_workflows_connector_endpoint_ip_addresses" {
-  description = "List of connector_endpoint_ip_addresses values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.connector_endpoint_ip_addresses]
+  description = "Map of connector_endpoint_ip_addresses values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.connector_endpoint_ip_addresses }
 }
 output "logic_app_workflows_connector_outbound_ip_addresses" {
-  description = "List of connector_outbound_ip_addresses values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.connector_outbound_ip_addresses]
+  description = "Map of connector_outbound_ip_addresses values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.connector_outbound_ip_addresses }
 }
 output "logic_app_workflows_enabled" {
-  description = "List of enabled values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.enabled]
+  description = "Map of enabled values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.enabled }
 }
 output "logic_app_workflows_identity" {
-  description = "List of identity values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.identity]
+  description = "Map of identity values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.identity }
 }
 output "logic_app_workflows_integration_service_environment_id" {
-  description = "List of integration_service_environment_id values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.integration_service_environment_id]
+  description = "Map of integration_service_environment_id values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.integration_service_environment_id }
 }
 output "logic_app_workflows_location" {
-  description = "List of location values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.location]
+  description = "Map of location values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.location }
 }
 output "logic_app_workflows_logic_app_integration_account_id" {
-  description = "List of logic_app_integration_account_id values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.logic_app_integration_account_id]
+  description = "Map of logic_app_integration_account_id values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.logic_app_integration_account_id }
 }
 output "logic_app_workflows_name" {
-  description = "List of name values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.name]
+  description = "Map of name values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.name }
 }
 output "logic_app_workflows_parameters" {
-  description = "List of parameters values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.parameters]
+  description = "Map of parameters values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.parameters }
 }
 output "logic_app_workflows_resource_group_name" {
-  description = "List of resource_group_name values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.resource_group_name]
+  description = "Map of resource_group_name values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.resource_group_name }
 }
 output "logic_app_workflows_tags" {
-  description = "List of tags values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.tags]
+  description = "Map of tags values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.tags }
 }
 output "logic_app_workflows_workflow_endpoint_ip_addresses" {
-  description = "List of workflow_endpoint_ip_addresses values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.workflow_endpoint_ip_addresses]
+  description = "Map of workflow_endpoint_ip_addresses values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.workflow_endpoint_ip_addresses }
 }
 output "logic_app_workflows_workflow_outbound_ip_addresses" {
-  description = "List of workflow_outbound_ip_addresses values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.workflow_outbound_ip_addresses]
+  description = "Map of workflow_outbound_ip_addresses values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.workflow_outbound_ip_addresses }
 }
 output "logic_app_workflows_workflow_parameters" {
-  description = "List of workflow_parameters values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.workflow_parameters]
+  description = "Map of workflow_parameters values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.workflow_parameters }
 }
 output "logic_app_workflows_workflow_schema" {
-  description = "List of workflow_schema values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.workflow_schema]
+  description = "Map of workflow_schema values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.workflow_schema }
 }
 output "logic_app_workflows_workflow_version" {
-  description = "List of workflow_version values across all logic_app_workflows"
-  value       = [for k, v in azurerm_logic_app_workflow.logic_app_workflows : v.workflow_version]
+  description = "Map of workflow_version values across all logic_app_workflows, keyed the same as var.logic_app_workflows"
+  value       = { for k, v in azurerm_logic_app_workflow.logic_app_workflows : k => v.workflow_version }
 }
 
